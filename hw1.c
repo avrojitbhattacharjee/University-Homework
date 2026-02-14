9e4d3dec6eaca69858a6ae7b856f3cd5ffc6f2bc
Avrojit Bhattacharjee <avrojitbhattacharjee309@gmail.com>
2026-02-14 23:50:02 +0600

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -33,6 +33,9 @@ int main()
     return 0;
 }
 
+
+
+
 //3.Write a C program to check whether a number is divisible by 5 and 11 or not.
 
 #include<stdio.h>
@@ -49,6 +52,9 @@ int main()
 
     return 0;
 }
+
+
+
 
 //4.Write a C program to input any alphabet and check whether it is vowel or consonant.
 
@@ -75,6 +81,8 @@ int main()
     return 0;
 }
 
+
+
 //5.Write a C program to input any character and check whether it is alphabet, digit or special character.
 
 #include<stdio.h>
@@ -95,6 +103,82 @@ int main()
 
     return 0;
 }
+
+
+
+
+//6.Write a C program to find all roots of a quadratic equation.
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,b,c,disc,root1,root2;
+    printf("Enter a,b,c Values : ");
+    scanf("%f %f %f",&a,&b,&c);
+    
+    disc = b*b-4*a*c;
+    if (disc>0)
+    {
+        root1 = (-b+sqrt(disc))/(2*a);
+        root2 = (-b-sqrt(disc))/(2*a);
+        printf("The roots are real and unequal = %0.2f and %0.2f",root1,root2);
+    } else if (disc==0)
+    {
+        root1 = (-b/(2*a));
+        printf("The roots are real and equal = %0.2f",root1);
+    } else 
+           printf("The roots are complex and imaginary");
+           
+          
+    return 0;
+
+    }
+
+
+
+
+//7.7.Write a C program to check whether a character is uppercase or lowercase alphabet.
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character :");
+    scanf("%c",&ch);
+    
+    if(ch>='A' && ch<='Z')
+    {
+        printf("This is upper case alphabet");
+        
+    }else if (ch>='a' && ch<='z')
+    {     printf("Lower case alphabet");
+    
+    }else 
+         printf("Invalid input");
+     
+    return 0;     
+}
+
+
+
+
+//8.write a c programme to count total number of notes in given number.
+#include<stdio.h>
+int main()
+{
+    int amount,count=0,i;
+    int notes[]={1000,500,200,100,50,20,10,5,2,1};
+    printf("Enter amount :");
+    scanf("%d",&amount);
+    
+    for(i = 0; i<10; i++)
+    {
+        count += amount/notes[i];
+        amount = amount % notes[i];
+    } 
+    printf("Total number of notes = %d",count);
+    return 0;     
+}
+
 
 
 
